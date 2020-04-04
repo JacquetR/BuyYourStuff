624bba305e31008f810df4e13b23e4bc9ca4949d
AmongTheDevil <AmongTheDevil@users.noreply.github.com>
2020-04-04 17:02:11 +0200

    Route + design page login and register

--- BuyYourStuff/src/Entity/Product.php
+++ BuyYourStuff/src/Entity/Product.php
@@ -42,12 +42,6 @@ class Product
      */
     private $categorie;
 
-    /**
-     * @ORM\ManyToOne(targetEntity="App\Entity\categorie", inversedBy="products")
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $categorie;
-
     public function getId(): ?int
     {
         return $this->id;
@@ -97,18 +91,6 @@ class Product
     public function setPrice(int $price): self
     {
         $this->price = $price;
-
-        return $this;
-    }
-
-    public function getCategorie(): ?categorie
-    {
-        return $this->categorie;
-    }
-
-    public function setCategorie(?categorie $categorie): self
-    {
-        $this->categorie = $categorie;
 
         return $this;
     }
